4e8296a817ee8947486a27a27cf8577730625c22
Bhushan123 <bhushan.jagtap.2013@gmail.com>
2018-11-28 12:23:46 -0800

    Initial commit

---
+++ src/main/java/com/loginpage/rest/webservices/restfulwebloginpageservices/RestfulWebLoginpageServicesApplication.java
@@ -0,0 +1,12 @@
+package com.loginpage.rest.webservices.restfulwebloginpageservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebLoginpageServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebLoginpageServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/loginpage/rest/webservices/restfulwebloginpageservices/RestfulWebLoginpageServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loginpage.rest.webservices.restfulwebloginpageservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebLoginpageServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
