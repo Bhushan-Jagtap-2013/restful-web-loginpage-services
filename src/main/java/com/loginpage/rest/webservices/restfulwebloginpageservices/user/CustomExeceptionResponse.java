e4d99c8ab339bdb2edfac49490345b692eeab671
Bhushan123 <bhushan.jagtap.2013@gmail.com>
2018-11-28 19:21:05 -0800

    Added customized exception response

---
+++ src/main/java/com/loginpage/rest/webservices/restfulwebloginpageservices/user/CustomExeceptionResponse.java
@@ -0,0 +1,28 @@
+package com.loginpage.rest.webservices.restfulwebloginpageservices.user;
+
+import java.util.Date;
+
+public class CustomExeceptionResponse {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public CustomExeceptionResponse(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
